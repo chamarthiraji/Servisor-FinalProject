e342ba390ec5b3dfcbc495b4b9f623826c07dca6
Rajyalakshmi C <chamarthi226@gmail.com>
2017-01-21 19:06:59 -0600

    search route added

--- app/config/routes.js
+++ app/config/routes.js
@@ -6,6 +6,10 @@ import Main from '../components/Main';
 import FirstPage from '../components/FirstPage';
 import SigninPage from '../components/signin/SigninPage'
 import RegisterPage from '../components/register/RegisterPage'
+import SearchPage from '../components/search/SearchPage'
+
+
+
 
 module.exports = (
   <Router history={browserHistory}>
@@ -13,6 +17,8 @@ module.exports = (
   	<IndexRoute	component={FirstPage} />
   	<Route path="signin" component={SigninPage} />
   	<Route path="register" component={RegisterPage} />
+  	<Route path="searchPage" component={SearchPage} />
+  	
     </Route>
   </Router>
 );
