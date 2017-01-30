56053281f021cab829662a777d767aaa69b7b2d0
Rajyalakshmi C <chamarthi226@gmail.com>
2017-01-30 08:44:12 -0600

    added dropdown buttons

--- app/Components/search/ProvidersPage.js
+++ app/Components/search/ProvidersPage.js
@@ -1,5 +1,6 @@
 import React,{Component} from 'react'
 import axios from 'axios'
+import { ButtonGroup,Button,DropdownButton,MenuItem } from 'react-bootstrap';
 
 import ProvidersListPage from './ProvidersListPage'
 
@@ -9,25 +10,60 @@ export default class ProvidersPage extends Component {
 			super(props);
 			console.log("ProvidersPage props",JSON.stringify(props.params));
 			this.state = {
-				providers: []
+				providers: [],
+				serviceName : undefined,
+				specializationName : undefined
 			}
+			this.serviceSelect = this.serviceSelect.bind(this);
+			this.getProviderData = this.getProviderData.bind(this);
 	}
 
 	componentDidMount() {
 		console.log("inside componentDidMount");
-		console.log("serviceName:"+this.props.params.serviceName);
+		//console.log("serviceName:"+this.props.params.serviceName);
 		
-		axios.get('/api/providers/' + this.props.params.serviceName).then(
+		//axios.get('/api/providers/' + this.props.params.serviceName).then(
+		this.getProviderData();
+	} 
+
+	getProviderData() {
+
+	 	axios.post('/api/providers',this.state).then(
 			providers => {
 				console.log("providers data",providers.data);
 				this.setState({ providers: providers.data });
 			}
 		);
-	} 
+
+	} // end of - getProviderData
+
+	serviceSelect (eventKey){
+	 	
+	 	console.log("serviceSelect evtKey",eventKey);
+	 	
+
+	 	var newState = this.state;
+		// console.log('state', newState);
+		newState["serviceName"]=eventKey;
+		this.setState(newState);
+
+		this.getProviderData();
+
+	}
 
 	render() {
 		return(
 			<div className="jumbotron">
+				<ButtonGroup justified>
+				    <Button href="#">Left</Button>
+				    <Button href="#">Middle</Button>
+				    <DropdownButton  onSelect={this.serviceSelect} title="Search by Service" id="bg-justified-dropdown">
+				      <MenuItem eventKey="Catering">Catering</MenuItem>
+				      <MenuItem eventKey="Food">Food</MenuItem>
+				      <MenuItem eventKey="Tutoring">Tutoring</MenuItem>
+				    </DropdownButton>
+				</ButtonGroup>
+
 				<ul>
 					{this.state.providers.map(provider =>
 					  <ProvidersListPage key={provider._id} provider={provider}/>
