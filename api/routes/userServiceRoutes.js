13ffe0e7344f004ea73373920d60a7e458bff49b
Rajyalakshmi C <chamarthi226@gmail.com>
2017-02-20 09:58:15 -0600

    nodemailer fixes

--- api/routes/userServiceRoutes.js
+++ api/routes/userServiceRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const nodemailer =require('nodemailer');
+// const nodemailer = require('nodemailer');
+var nodemailer = require('nodemailer-smtp-transport');
 
 var serviceProviders = require('../../api/models/serviceProviders');
 var Reviews = require('../../api/models/Review');
