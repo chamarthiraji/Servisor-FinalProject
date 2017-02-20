2cc52805307ff56cc817fd543b0376c975aced0c
Rajyalakshmi C <chamarthi226@gmail.com>
2017-02-20 00:06:07 -0600

    cleanup

--- api/routes/userServiceRoutes.js
+++ api/routes/userServiceRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-var nodemailer =require('nodemailer');
+const nodemailer =require('nodemailer');
 
 var serviceProviders = require('../../api/models/serviceProviders');
 var Reviews = require('../../api/models/Review');
